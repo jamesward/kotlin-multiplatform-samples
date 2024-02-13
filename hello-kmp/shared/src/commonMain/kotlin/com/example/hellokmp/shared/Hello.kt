b0bd9f832fbbe55b75e009d56a25dbb4638a5fd2
James Ward <james@jamesward.com>
2024-02-13 15:38:01 -0700

    no expect/actuals

--- hello-kmp/shared/src/commonMain/kotlin/com/example/hellokmp/shared/Hello.kt
+++ hello-kmp/shared/src/commonMain/kotlin/com/example/hellokmp/shared/Hello.kt
@@ -1,3 +1,11 @@
 package com.example.hellokmp.shared
 
-fun hello() = "hello, world"
+// implementation shared across all target platforms
+fun hello(): String = "hello, world"
+
+// or if you need different platform implementations:
+//    remove the previous `fun hello()`
+//    uncomment the `expect fun hello()`
+//    use the quick-fix "add missing actual declarations"
+//    to add the actual definitions for your target platforms
+// expect fun hello(): String
